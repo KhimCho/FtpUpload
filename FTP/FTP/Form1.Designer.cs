30f9a3527da411cfb9eb056dd9a387284479e4ad
KhimCho <95128995+KhimCho@users.noreply.github.com>
2023-05-03 21:15:16 +0700

    Add files via upload

---
+++ FTP/FTP/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serv = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnUpld = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serv
+            // 
+            this.serv.AutoSize = true;
+            this.serv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serv.Location = new System.Drawing.Point(177, 81);
+            this.serv.Name = "serv";
+            this.serv.Size = new System.Drawing.Size(59, 20);
+            this.serv.TabIndex = 0;
+            this.serv.Text = "Server:";
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user.Location = new System.Drawing.Point(163, 123);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(87, 20);
+            this.user.TabIndex = 0;
+            this.user.Text = "Username:";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(168, 169);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(82, 20);
+            this.pass.TabIndex = 0;
+            this.pass.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(282, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(282, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(220, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(193, 292);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(378, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // btnUpld
+            // 
+            this.btnUpld.Location = new System.Drawing.Point(337, 240);
+            this.btnUpld.Name = "btnUpld";
+            this.btnUpld.Size = new System.Drawing.Size(75, 23);
+            this.btnUpld.TabIndex = 3;
+            this.btnUpld.Text = "Upload";
+            this.btnUpld.UseVisualStyleBackColor = true;
+            this.btnUpld.Click += new System.EventHandler(this.btnUpld_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(333, 350);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(109, 20);
+            this.status.TabIndex = 4;
+            this.status.Text = "Uploaded: ?%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 420);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.btnUpld);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.serv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label serv;
+        private System.Windows.Forms.Label user;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnUpld;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ FTP/FTP/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Ftp
+        {
+            public string Serv { get; set; }
+            public string User { get; set; }
+            public string Pass { get; set; }
+            public string Filename { get; set; }
+            public string FullName { get; set; }
+
+        }
+
+        Ftp input;
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string fileName = ((Ftp)e.Argument).Filename;
+            string fULLName = ((Ftp)e.Argument).FullName;
+            string Username = ((Ftp)e.Argument).User;
+            string Password = ((Ftp)e.Argument).Pass;
+            string Server  = ((Ftp)e.Argument).Serv;
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, fileName)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(Username, Password);
+            Stream FtpStream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(fULLName);
+            byte[] buffer = new byte[1024];
+            double total = (double)fs.Length;
+            int byteRead = 0;
+            double read = 0;
+            do
+            {
+                if (!backgroundWorker.CancellationPending)
+                {
+                    byteRead = fs.Read(buffer, 0, 1024);
+                    FtpStream.Write(buffer, 0, 1024);
+                    read += (double)byteRead;
+                    double percentage = read / total * 100;
+                    backgroundWorker.ReportProgress((int)percentage);
+                }
+            }
+            while (byteRead != 0);
+            fs.Close();
+            FtpStream.Close();
+
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            status.Text = $"Uploaded: {e.ProgressPercentage}";
+            progressBar1.Value = e.ProgressPercentage;
+            progressBar1.Update();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            status.Text = "Upload completed!";
+        }
+
+        private void btnUpld_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect= false, ValidateNames = true, Filter= "All files|*.*"})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi = new FileInfo(ofd.FileName);
+                    input.User = user.Text;
+                    input.Pass = pass.Text;
+                    input.Serv = serv.Text;
+                    input.Filename = fi.Name;
+                    input.FullName = fi.FullName;
+                }
+            }
+        }
+    }
+}
